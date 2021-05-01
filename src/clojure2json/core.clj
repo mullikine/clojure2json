29ffea3aed1bac502c8b181250675a7803ad041e
Shane Mulligan <mullikine@gmail.com>
2021-05-01 13:14:05 +1200

    Sat 01 May 2021 13:14:05

--- src/clojure2json/core.clj
+++ src/clojure2json/core.clj
@@ -1,9 +1,15 @@
 (ns clojure2json.core
   (:gen-class))
 
+(require '[clojure.pprint :as p])
 (require '[clojure.data.json :as json])
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
+  [fp & args]
+  (let [code (slurp fp)]
+    [json (json/write-str code)]
+    (p/pprint json))
+
+
   (println "Hello, World!"))
