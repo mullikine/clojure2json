b2ca77c893c34f9724a5d49f3cec0c1b100bda43
Shane Mulligan <mullikine@gmail.com>
2021-05-01 13:26:37 +1200

    Sat 01 May 2021 13:26:37

--- src/clojure2json/core.clj
+++ src/clojure2json/core.clj
@@ -3,11 +3,18 @@
 
 (require '[clojure.pprint :as p])
 (require '[clojure.data.json :as json])
+(use '[clojure.java.shell :only [sh]])
+
+(defn tv
+  ""
+  [s]
+  (sh "tv" :in s)
+  s)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [fp & args]
   (let [code (slurp fp)
-        json (json/write-str (read-string code))]
+        json (json/write-str (read-string (tv code)))]
     ;; (p/pprint json)
     (print json)))
