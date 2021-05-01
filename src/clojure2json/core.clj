6f26cf5175337e2ab523e35f449fcf7e43ddda59
Shane Mulligan <mullikine@gmail.com>
2021-05-01 15:05:17 +1200

    Sat 01 May 2021 15:05:17

--- src/clojure2json/core.clj
+++ src/clojure2json/core.clj
@@ -5,19 +5,28 @@
 (require '[clojure.data.json :as json])
 (use '[clojure.java.shell :only [sh]])
 
+;; This appears to hang
 (defn tv
   ""
   [s]
   (sh "tv" :in s)
   s)
 
+;; (defn pprint-to-string
+;;   ""
+;;   [o]
+;;   (with-out-str (clojure.pprint/pprint o )))
+;; (def pps pprint-to-string)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [fp & args]
   (let [code (slurp fp)
+        ;; pp (p/pprint
+        ;;     (tv code)
+        ;;     code)
         json (json/write-str (read-string
-                              (tv code)
-                              ;; code
-                              ))]
-    ;; (p/pprint json)
+                              ;; (tv code)
+                              code))]
+    (p/pprint json)
     (print json)))
